658db8590bb8d33925ccda8c70d4c54f84b3d760
Nikita Bansode <bansodenikita66@gmail.com>
2026-03-02 11:53:42 +0530

    React components for skill taxonomy

---
+++ skill_taxonomy/src/App.jsx
@@ -0,0 +1,14 @@
+import Subject from "./components/Subject"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Subject />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ skill_taxonomy/src/components/Subject.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Subject() {
+   
+    return(
+        <div>
+            SUBJECT MANAGEMENT COMPONENT 📚<br/><br/>
+            
+                <button class="btn btn-primary me-2">Add Subject</button>
+                <button class="btn btn-primary me-2">Remove Subject</button>
+                <button class="btn btn-primary me-2">Display Subject</button>
+                <button class="btn btn-primary me-2">Update Subject</button>
+        </div>
+       
+    );
+}
+export default Subject;
+
+// npm install react-bootstrap bootstrap
