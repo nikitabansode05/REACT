e1123244f2069be68e514cf9396e3de9bf15f9bd
Nikita Bansode <bansodenikita66@gmail.com>
2026-04-05 22:55:41 +0530

    useState and event handler use in chatbot

--- chatbot/src/components/ChatMessageList.jsx
+++ chatbot/src/components/ChatMessageList.jsx
@@ -1,20 +1,34 @@
+import { useState } from "react";
 import ChatMessage from "./ChatMessage";
 
-const messages=[{message:"hii" , sender:"user"},
-                    {message:"hello", sender:"bot"},
-                    {message:"How are you ??",sender:"user"},
-                    {message:"Pretty Good",sender:"bot"}
-                ];
-                
- const chatMessageComponents=messages.map((chatMessage)=>{
-        return(
-            <ChatMessage message={chatMessage.message} sender={chatMessage.sender}/>
-        );
-    });
+
 
 const ChatMessageList=()=>{
+    const [messages,setMessages]=useState([{message:"hii" , sender:"user",id:crypto.randomUUID()},
+                                       {message:"hello", sender:"bot",id:crypto.randomUUID()},
+                                       {message:"How are you ??",sender:"user",id:crypto.randomUUID()},
+                                       {message:"Pretty Good",sender:"bot",id:crypto.randomUUID()}
+                ]);
+                
+    const chatMessageComponents=messages.map((chatMessage)=>{
+            return(
+                <ChatMessage message={chatMessage.message} sender={chatMessage.sender} key={chatMessage.id}/>
+            );
+        });
+
+    function generateMessage(){
+        setMessages([ ...messages,
+            {
+                message:"Ohh Great",
+                sender:"user",
+                id:crypto.randomUUID()
+            }]);
+    }
+
+
     return(
         <div>
+            <button onClick={generateMessage}>Send Message</button>
             {chatMessageComponents}
         </div>
     );
